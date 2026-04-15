7669634273ace78bc4d907d0c43799c3128c6cab
enockdeghost <enockdeghost@gmail.com>
2026-04-15 13:23:59 +0300

    type

---
+++ type.js
@@ -0,0 +1,34 @@
+// data type in javascript is a type of value that can be stored and manipulated in a program.
+// there are 7 data types in javascript:
+// 1. string
+// 2. number
+// 3. boolean
+// 4. null
+// 5. undefined
+// 6. symbol
+// 7. object
+
+// string data type is a sequence of characters enclosed in single or double quotes.
+let name = "John Doe";      
+
+// number data type is a numeric value that can be an integer or a floating-point number.
+let age = 30;   
+
+// boolean data type is a logical value that can be either true or false.
+let isStudent = true;
+
+// null data type is a special value that represents the absence of any value.
+let address = null;
+
+// undefined data type is a special value that represents the absence of a value or an uninitialized variable.
+let phoneNumber;
+
+// symbol data type is a unique and immutable value that can be used as a key for object properties.
+let id = Symbol('id');
+
+// object data type is a collection of properties and methods that can be used to store and manipulate data.
+let person = {
+  name: "John Doe",
+  age: 30,
+  isStudent: true
+};   
