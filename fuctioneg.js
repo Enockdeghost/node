01e16eedca3bdea9dad1224c56b2696d17aeb88b
enockdeghost <enockdeghost@gmail.com>
2026-04-24 15:39:42 +0300

    add new str conc

--- fuctioneg.js
+++ fuctioneg.js
@@ -1,10 +1,1 @@
-let student = {
-  name: "Amina",
-  age: 21
-}
-student.grade = 10
-
-for (let key in student) {
-  console.log(key + ": " + student[key])
-}
-// console.log(student)
+// link js file
---
+++ script.js
@@ -0,0 +1,22 @@
+// alert('its working')
+
+const name = 'John';
+const age = 30;
+const year = 'third year';
+const bord = 1950;
+const now = 2026;
+
+console.log(`My name is ${name} and I am ${age} years old. I am in my ${year}. I was born in ${bord} and the current year is ${now}.`);
+
+
+const S = "one line \n\
+another line \n\
+and another one";
+
+console.log(S);
+
+const S2 = `one line
+another line
+and another one`;
+
+console.log(S2);
