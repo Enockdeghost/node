b4034e9ff77af5ac9a8893574e64073f76e18ab1
enockdeghost <enockdeghost@gmail.com>
2026-05-05 15:51:04 +0300

    Quize

---
+++ ifandelsequize.js
@@ -0,0 +1,42 @@
+month = "January"
+month1 = month.toLowerCase()
+switch(month1) {
+    case "january":
+        console.log("summer time");
+        break;
+    case "february":
+        console.log("summer time");
+        break;
+    case "march":
+        console.log("summer time");
+        break;
+    case "april":
+        console.log("autumn time");
+        break;
+    case "may":
+        console.log("autumn time");
+        break;
+    case "june":
+        console.log("autumn time");
+        break;
+    case "july":
+        console.log("winter time");
+        break;
+    case "august":
+        console.log("winter time");
+        break;
+    case "september":
+        console.log("winter time");
+        break;
+    case "october":
+        console.log("spring time");
+        break;
+    case "november":
+        console.log("spring time");
+        break;
+    case "december":
+        console.log("spring time");
+        break;
+    default:
+        console.log("invalid month")
+}
