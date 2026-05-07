bc39a16db8cf5ee2527ca7b07abd0c1da19efa96
enockdeghost <enockdeghost@gmail.com>
2026-05-07 15:50:28 +0300

    Changes

--- case.js
+++ case.js
@@ -1,51 +1,29 @@
-const age = 101
+const age = 6
 
-if (age >=0 && age <= 80) {
-    switch (age >= 0 && age <= 12) {
-        case true:
-            console.log("You are a child");
-            break;
-       case age >= 13 && age <= 19:
-            console.log("You are a teenager");
-            break;
+
+
+switch (age >=0 && age <= 80) {
+    case (age):
+        case age >= 0 && age <= 12:
+        console.log("You are a child");
+        break;
+    case (age):
+        case age >= 13 && age <= 19:
+        console.log("You are a teenager");
+        break;
+    case (age):
         case age >= 20 && age <= 35:
-            console.log("You are a young adult");
-            break;
+        console.log("You are a young adult");
+        break;
+    case (age ):
         case age >= 36 && age <= 60:
-            console.log("You are an adult");
-            break;
+        console.log("You are an adult");
+        break;
+    case (age) :
         case age >= 61 && age <= 80:
-            console.log("You are a senior citizen");
-            break;
-        default:
-            console.log("Invalid age");
-    }
-
-
-
-}
-else {
+        console.log("You are a senior citizen");
+        break;
+    
+default:
     console.log("Invalid age");
 }
-
-
-// switch (age >=0 && age <= 80) {
-//     case (age >= 0 && age <= 12):
-//         console.log("You are a child");
-//         break;
-//     case (age >= 13 && age <= 19):
-//         console.log("You are a teenager");
-//         break;
-//     case (age >= 20 && age <= 35):
-//         console.log("You are a young adult");
-//         break;
-//     case (age >= 36 && age <= 60):
-//         console.log("You are an adult");
-//         break;
-//     case (age >= 61 && age <= 80):
-//         console.log("You are a senior citizen");
-//         break;
-    
-// default:
-//     console.log("Invalid age");
-// }
